78e383d22743bd0424c7bcf0d340b7d0b805763d
hemn-one398~ <47257299+hemn-one398@users.noreply.github.com>
2023-03-03 17:11:53 +0300

    Last commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import Card from "./components/Card";
 import Navbar from "./components/Navbar";
-
+///
 const regionOptions = [
   { value: "AllRegions", label: "AllRegions" },
   { value: "Africa", label: "Africa" },
